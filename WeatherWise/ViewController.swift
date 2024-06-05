93b933c2a6fd19556954351a16382e4b5c61ff8d
Anup Saud <anupsaud10@gmail.com>
2024-06-05 14:05:26 -0400

    Work on UI

--- WeatherWise/ViewController.swift
+++ WeatherWise/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setGradientBackground()
     }
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor.systemBlue.cgColor
+        let colorBottom = UIColor.systemTeal.cgColor
+                
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
 
 
 }
